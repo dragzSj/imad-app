28ab83301fa5472c238481128671fc3db170999e
dragzSj <30707474+dragzSj@users.noreply.github.com>
2017-08-14 01:01:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,6 +8,14 @@ element.innerHTML = 'NewerValue';
 //move img
 var img = document.getElementById('img');
 
-img.onclick = function (){
-    img.style.marginLeft = '100px';
+var marginLeft=0; 
+
+function moveRight () {
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
+
+img.onclick = function () {
+    //img.style.marginLeft = '100px';
+    var interval = setInterval(moveRight, 100);
 }
