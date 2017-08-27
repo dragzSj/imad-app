f23ecd6cb702193b309e263522001fc88a64b171
dragzSj <30707474+dragzSj@users.noreply.github.com>
2017-08-27 23:04:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var crypto = require('crypto');
 
 var config = {
     user: 'dragzsj',
@@ -52,6 +53,17 @@ function createTemplate (data){
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+function hash (input, salt) {
+  //create a hash
+  var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+  return ["pbkdf2", "10000", salt, hashed.toString('hex')].join('$');    
+}
+
+app.get('/hash/:input', function(req, res) {
+  var hashedString = hash(req.params.input,'salt-string-value');
+  res.send(hashedString);
 });
 
 var pool = new Pool(config);
