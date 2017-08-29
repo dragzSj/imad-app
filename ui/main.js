c63fab9385ac4527ff1b68de334aa2ed352213f5
dragzSj <30707474+dragzSj@users.noreply.github.com>
2017-08-30 00:07:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -166,9 +166,9 @@ function loadLogin () {
     request.onreadystatechange = function () {
         if (request.readyState === XMLHttpRequest.DONE) {
             if (request.status === 200) {
-                loadLoginForm();
-            } else {
                 loadLoggedInUser(this.responseText);
+            } else {
+                loadLoginForm();
             }
         }
     };
