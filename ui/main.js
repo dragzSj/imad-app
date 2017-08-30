df41791e1a9c4b0efd36655330babb5b7010b820
dragzSj <30707474+dragzSj@users.noreply.github.com>
2017-08-31 03:42:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -201,7 +201,7 @@ function loadArticles () {
         }
     };
     
-    request.open('GET', '/article/articleName', true);
+    request.open('GET', '/article', true);
     request.send(null);
 }
 
